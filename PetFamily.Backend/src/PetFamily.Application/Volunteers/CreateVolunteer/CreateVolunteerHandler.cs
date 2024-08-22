51f6d7c2b73920f88c9d20ef18e8cbc0030c671c
Alexandr <bagnencko.sasha1@gmail.com>
2024-08-22 13:49:48 +0300

    refactor CreateVolunteerHandler.cs

--- PetFamily.Backend/src/PetFamily.Application/Volunteers/CreateVolunteer/CreateVolunteerHandler.cs
+++ PetFamily.Backend/src/PetFamily.Application/Volunteers/CreateVolunteer/CreateVolunteerHandler.cs
@@ -13,8 +13,6 @@ public class CreateVolunteerHandler(IVolunteersRepository repository)
     )
     {
         var phoneNumber = PhoneNumber.Create(request.Number);
-        if (phoneNumber.IsFailure)
-            return phoneNumber.Error;
 
         var volunteer = await repository.GetByPhoneNumber(phoneNumber.Value);
 
@@ -24,32 +22,19 @@ public class CreateVolunteerHandler(IVolunteersRepository repository)
         var volunteerId = VolunteerId.NewId();
 
         var fullName = FullName.Create(request.Name, request.Surname, request.Patronymic);
-        if (fullName.IsFailure)
-            return fullName.Error;
 
         var description = Description.Create(request.Description);
-        if (description.IsFailure)
-            return description.Error;
 
         var ageExperience = AgeExperience.Create(request.AgeExperience);
-        if (ageExperience.IsFailure)
-            return ageExperience.Error;
 
         var socialLinks = request.SocialLinks
             .Select(x => SocialLink.Create(x.Name, x.Url))
             .ToList();
 
-        var firstSocialLinkError = socialLinks.FirstOrDefault(x => x.IsFailure);
-        if (firstSocialLinkError.IsFailure)
-            return firstSocialLinkError.Error;
-
         var requisites = request.Requisites
             .Select(x => Requisite.Create(x.Name, x.Description))
             .ToList();
-        var firstRequisiteError = requisites.FirstOrDefault(x => x.IsFailure);
-        if (firstRequisiteError.IsFailure)
-            return firstRequisiteError.Error;
-
+        
         var details = new VolunteerDetails(socialLinks.Select(x => x.Value).ToList(),
             requisites.Select(x => x.Value).ToList());
 
