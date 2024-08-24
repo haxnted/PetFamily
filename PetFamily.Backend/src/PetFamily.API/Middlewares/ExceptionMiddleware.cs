b941f6542715c1e8ee3bee34a15b4c3eccee0907
Alexandr <bagnencko.sasha1@gmail.com>
2024-08-24 21:03:24 +0300

    Add custom exception middleware

---
+++ PetFamily.Backend/src/PetFamily.API/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using PetFamily.API.Response;
+
+namespace PetFamily.API.Middlewares;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ExceptionMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var responseError = new ResponseError("server.internal_error", ex.Message, null);
+            var envelope = Envelope.Error([responseError]);
+
+            await context.Response.WriteAsJsonAsync(envelope);
+        }
+    }
+}
+
+public static class ExceptionMiddlewareExtensions
+{
+    public static IApplicationBuilder UseExceptionMiddleware(this IApplicationBuilder builder)
+        => builder.UseMiddleware<ExceptionMiddleware>();
+}
