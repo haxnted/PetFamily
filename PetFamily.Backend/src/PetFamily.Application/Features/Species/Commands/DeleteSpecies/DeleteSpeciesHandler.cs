0f7d9fa0f6a1ecdc4696f7b2030ecb08e19d4e7e
Alexandr <bagnencko.sasha1@gmail.com>
2024-09-24 22:27:13 +0300

    realize feature "delete species"

---
+++ PetFamily.Backend/src/PetFamily.Application/Features/Species/Commands/DeleteSpecies/DeleteSpeciesHandler.cs
@@ -0,0 +1,35 @@
+﻿using CSharpFunctionalExtensions;
+using FluentValidation;
+using Microsoft.Extensions.Logging;
+using PetFamily.Application.Abstractions;
+using PetFamily.Application.Extensions;
+using PetFamily.Application.Features.Species.Commands.AddSpecies;
+using PetFamily.Domain.Shared;
+using PetFamily.Domain.Shared.EntityIds;
+
+namespace PetFamily.Application.Features.Species.Commands.DeleteSpecies;
+
+public class DeleteSpeciesHandler(
+    ISpeciesRepository speciesRepository,
+    ILogger<AddSpeciesHandler> logger,
+    IValidator<DeleteSpeciesCommand> validator) : ICommandHandler<Guid, DeleteSpeciesCommand>
+{
+    public async Task<Result<Guid, ErrorList>> Execute(DeleteSpeciesCommand command,
+        CancellationToken cancellationToken)
+    {
+        var validationResult = await validator.ValidateAsync(command, cancellationToken);
+        if (validationResult.IsValid == false)
+            return validationResult.ToList();
+
+        var speciesId = SpeciesId.Create(command.SpeciesId);
+        var species = await speciesRepository.GetSpeciesById(speciesId, cancellationToken);
+        if (species.IsFailure)
+            return species.Error.ToErrorList();
+
+        await speciesRepository.Delete(species.Value, cancellationToken);
+        
+        logger.Log(LogLevel.Information, "Species deleted successfully {Species}.", species.Value);
+
+        return speciesId.Id;
+    }
+}
