c4157674db69c53b8a3e97b2acdde30732eae3af
Alexandr <bagnencko.sasha1@gmail.com>
2024-09-24 22:24:14 +0300

    realize feature "add breed to species"

---
+++ PetFamily.Backend/src/PetFamily.API/Controllers/SpeciesController/Requests/AddBreedToSpeciesRequest.cs
@@ -0,0 +1,9 @@
+﻿using PetFamily.Application.Features.Species.Commands.AddBreedToSpecies;
+
+namespace PetFamily.API.Controllers.SpeciesController.Requests;
+
+public record AddBreedToSpeciesRequest(string Breed)
+{
+    public AddBreedToSpeciesCommand ToCommand(Guid speciesId) =>
+        new(speciesId, Breed);
+}
