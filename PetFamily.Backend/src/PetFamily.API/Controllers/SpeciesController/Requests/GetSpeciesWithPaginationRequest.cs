cc4905f2e5fa55bb0157127443cc2dae10dcdada
Alexandr <bagnencko.sasha1@gmail.com>
2024-09-24 22:29:37 +0300

    realize feature "get species with pagination"

---
+++ PetFamily.Backend/src/PetFamily.API/Controllers/SpeciesController/Requests/GetSpeciesWithPaginationRequest.cs
@@ -0,0 +1,12 @@
+﻿using PetFamily.Application.Features.Species.Queries.GetAllSpeciesWithPagination;
+
+namespace PetFamily.API.Controllers.SpeciesController.Requests;
+
+public record GetSpeciesWithPaginationRequest(
+    string? SortDirection,
+    int Page,
+    int PageSize)
+{
+    public GetSpeciesWithPaginationQuery ToQuery() =>
+        new(SortDirection, Page, PageSize);
+}
---
+++ PetFamily.Backend/src/PetFamily.Application/Features/Species/Queries/GetAllSpeciesWithPagination/GetSpeciesWithPaginationQuery.cs
@@ -0,0 +1,8 @@
+﻿using PetFamily.Application.Abstractions;
+
+namespace PetFamily.Application.Features.Species.Queries.GetAllSpeciesWithPagination;
+
+public record GetSpeciesWithPaginationQuery(
+    string? SortDirection,
+    int Page,
+    int PageSize) : IQuery;
