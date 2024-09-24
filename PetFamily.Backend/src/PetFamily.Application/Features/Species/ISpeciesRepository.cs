bf0476807be288864f71d42dd3d759b4f00f4450
Alexandr <bagnencko.sasha1@gmail.com>
2024-09-24 22:13:54 +0300

    add speciesRepository and controller

---
+++ PetFamily.Backend/src/PetFamily.Application/Features/Species/ISpeciesRepository.cs
@@ -0,0 +1,27 @@
+﻿using CSharpFunctionalExtensions;
+using PetFamily.Domain.Shared;
+using PetFamily.Domain.Shared.EntityIds;
+using PetFamily.Domain.VolunteerManagement.ValueObjects;
+
+namespace PetFamily.Application.Features.Species;
+
+public interface ISpeciesRepository
+{
+    public Task<Result<Guid, Error>> Add(
+        Domain.Species.Species species,
+        CancellationToken cancellationToken = default);
+
+    public Task<Result<Domain.Species.Species, Error>> GetSpeciesById(
+        SpeciesId id,
+        CancellationToken cancellationToken = default);
+
+    public Task<Result<Domain.Species.Species, Error>> GetSpeciesByName(
+        TypeAnimal animal, 
+        CancellationToken cancellationToken);
+
+    public Task<Result<Guid, Error>> Save(
+        Domain.Species.Species species, CancellationToken cancellationToken = default);
+
+    public Task<Result<Guid, Error>> Delete(
+        Domain.Species.Species species, CancellationToken cancellationToken = default);
+}
