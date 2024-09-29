c87f3eaf169c555357f6fd6c74fbd3c0c04cf451
Alexandr <bagnencko.sasha1@gmail.com>
2024-09-29 13:55:21 +0300

    realize feature UpdateGeneralPetInfo

---
+++ PetFamily.Backend/src/PetFamily.API/Controllers/Volunteers/Requests/UpdateGeneralPetInfoRequest.cs
@@ -0,0 +1,41 @@
+﻿using PetFamily.Application.Dto;
+using PetFamily.Application.Features.VolunteerManagement.Commands.UpdateGeneralPetInfo;
+using PetFamily.Domain.VolunteerManagement.Enums;
+
+namespace PetFamily.API.Controllers.Volunteers;
+
+public record UpdateGeneralPetInfoRequest(
+    string NickName,
+    string GeneralDescription,
+    string HealthDescription,
+    AddressDto Address,
+    double Weight,
+    double Height,
+    string PhoneNumber,
+    DateTime BirthDate,
+    Guid SpeciesId,
+    Guid BreedId,
+    bool IsCastrated,
+    bool IsVaccinated,
+    HelpStatusPet HelpStatus,
+    IEnumerable<RequisiteDto> Requisites)
+{
+    public UpdateGeneralPetInfoCommand ToCommand(Guid Volunteer)
+    {
+        return new UpdateGeneralPetInfoCommand(Volunteer,
+            NickName,
+            GeneralDescription,
+            HealthDescription,
+            Address,
+            Weight,
+            Height,
+            PhoneNumber,
+            BirthDate,
+            SpeciesId,
+            BreedId,
+            IsCastrated,
+            IsVaccinated,
+            HelpStatus,
+            Requisites);
+    }
+}
