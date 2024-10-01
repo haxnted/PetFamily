1321a177ae3e01a807bc42614a7bb9436053a1ab
Alexandr <bagnencko.sasha1@gmail.com>
2024-10-01 17:10:46 +0300

    feat(RemoveSoftPetByIdHandler): realize feature

---
+++ PetFamily.Backend/src/PetFamily.API/Controllers/Volunteers/Requests/RemoveSoftPetByIdRequest.cs
@@ -0,0 +1,9 @@
+﻿using PetFamily.Application.Features.VolunteerManagement.Commands.RemoveSoftPetById;
+
+namespace PetFamily.API.Controllers.Volunteers;
+
+public record RemoveSoftPetByIdRequest(Guid PetId)
+{
+    public RemoveSoftPetByIdCommand ToCommand(Guid volunteerId) => 
+        new(volunteerId, PetId);
+}
---
+++ PetFamily.Backend/src/PetFamily.Application/Features/VolunteerManagement/Commands/RemoveSoftPetById/RemoveSoftPetByIdCommand.cs
@@ -0,0 +1,5 @@
+﻿using PetFamily.Application.Abstractions;
+
+namespace PetFamily.Application.Features.VolunteerManagement.Commands.RemoveSoftPetById;
+
+public record RemoveSoftPetByIdCommand(Guid VolunteerId, Guid PetId) : ICommand;
